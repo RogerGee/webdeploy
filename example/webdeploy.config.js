e81abac946c366327ff9741b906b70dc0c1d05f9
Roger Gee <roger.gee@tulsalibrary.org>
2018-02-27 12:28:08 -0600

    Update docs and example files

--- example/webdeploy.config.js
+++ example/webdeploy.config.js
@@ -14,34 +14,55 @@
 
 const CONFIG = {
     /**
-     * Rules define how targets are mapped to their deployment variants.
+     * Specifies which deployment plugin is to be loaded. You should specify
+     * something here since the default plugin is "exclude" (which excludes
+     * everything).
      */
-    rules: [
-        pattern: {
-            /**
-             * Tells the deployer which deployment strategy to use. This
-             * corresponds to a deployment plugin. This should be specified for
-             * any rule since the default deployment is "exclude".
-             */
-            deployment: "match",
+    deployment: "write",
 
-            /**
-             * Handlers operate on targets and produce the transformed
-             * outputs. They are invoked in sequence. This may be a custom
-             * handler function or a string denoting an installed handler
-             * plugin.
-             *
-             * By default, a handler is not made available during a development
-             * run. If this is required, then an object should be specified that
-             * has the schema { handler, dev } with dev=true.
-             */
-            handlers: [
-                "handler-name",
-                function(context,stream) {
-                    // ...
-                },
-                { handler: "handler-name", dev: true }
-            ]
+    /**
+     * Specifies which handler plugins are to be loaded for the build
+     * process. This list may be empty if nothing is to be built.
+
+     * Handlers operate on targets and produce the transformed
+     * outputs. They are invoked in sequence. This may be a custom
+     * handler function or a string denoting an installed handler
+     * plugin.
+     *
+     * By default, a handler is not made available during a development
+     * run. If this is required, then set "dev" to true.
+     */
+    handlers: [
+        {
+            // The name ID of the handler plugin. If the handler is inline, then
+            // this name is used to register the inline handler. An inline
+            // handler takes precendence over any external handler plugins.
+            plugin: "handler-name",
+
+            // Determines if the handler is considered during a development
+            // run. The default is false.
+            dev: false,
+
+            // A custom, inline plugin implementation.
+            handler: (target) => {
+
+            }
+        }
+    ],
+
+    /**
+     * Defines which blobs are included as targets in the deployment.
+     */
+    includes: [
+        {
+            // Specifies the pattern that matches the path names of candidate
+            // targets.
+            pattern: /.*\.js$/,
+
+            // Specifies any loaded handler plugins to explicitly ignore. This
+            // can be omitted, empty or false to include all handlers. A value
+            // of "true" excludes all handlers.
+            excludeHandlers: [],
         }
     ]
 };
