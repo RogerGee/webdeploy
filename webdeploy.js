0c1ca7e31b2439ad920f150a11653b3006f0fe58
Roger Gee <roger.gee@tulsalibrary.org>
2018-02-27 18:02:58 -0600

    Add code for build step

--- webdeploy.js
+++ webdeploy.js
@@ -5,11 +5,10 @@
 var commander = require("commander");
 var nodegit = require("nodegit");
 var path = require("path");
-var builder = require("./lib/builder");
 var deployer = require("./lib/deployer");
 
-function deploy_server(cmd) {
-    throw new Error("webdeploy server mode is not implemented yet");
+function reject(err) {
+    console.log("[FAIL] " + String(err));
 }
 
 commander.version("0.0.0","-v, --version");
@@ -17,22 +16,46 @@ commander.version("0.0.0","-v, --version");
 commander.command("deploy")
     .option("-d, --dry-run","Perform dry run")
     .action((cmd) => {
-        deployer.deployLocal(path.resolve("."),cmd.dryRun ? cmd.dryRun : false);
+        var options = {
+            dryRun: cmd.dryRun ? true : false,
+            type: deployer.types.TYPE_DEPLOY
+        };
+
+        deployer.deployLocal(path.resolve("."),options).then(() => {
+            console.log("DONE");
+        }, reject);
     });
 
 commander.command("deploy <repo>")
     .option("-d, --dry-run","Perform dry run")
     .action((repo,cmd) => {
-        deployer.deployRepo(repo,cmd.dryRun ? cmd.dryRun : false);
+        var options = {
+            dryRun: cmd.dryRun ? true : false,
+            type: deployer.types.TYPE_DEPLOY
+        };
+
+        deployer.deployRepository(repo,options).then(() => {
+            console.log("DONE");
+        }, reject);
     });
 
 commander.command("build")
+    .option("-d, --dry-run","Perform dry run")
     .action((cmd) => {
-        builder.buildPath(path.resolve("."));
+        var options = {
+            dryRun: cmd.dryRun ? true : false,
+            type: deployer.types.TYPE_BUILD
+        };
+
+        deployer.deployLocal(path.resolve("."),options).then(() => {
+            console.log("DONE");
+        }, reject);
     });
 
 commander.command("server")
     .option("-p, --path","Unix domain socket path")
-    .action(deploy_server);
+    .action((cmd) => {
+        throw new Error("webdeploy server mode is not implemented yet");
+    });
 
 commander.parse(process.argv);
