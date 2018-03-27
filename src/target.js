86721c7b2f00180fcdd0620bf9224678f806593d
Roger Gee <roger.gee@tulsalibrary.org>
2018-03-27 16:51:28 -0500

    target: Change Target into ES6 class

--- src/target.js
+++ src/target.js
@@ -21,96 +21,105 @@ function makeOutputTarget(newTargetPath,newTargetName,options) {
     return newTarget;
 }
 
-function Target(sourcePath,targetName,stream,options) {
-    // Ensure the sourcePath is never an absolute path.
-    if (pathModule.isAbsolute(sourcePath)) {
-        throw new Error("Target sourcePath cannot be an absolute path");
+class Target {
+    constructor(sourcePath,targetName,stream,options) {
+        // Ensure the sourcePath is never an absolute path.
+        if (pathModule.isAbsolute(sourcePath)) {
+            throw new Error("Target sourcePath cannot be an absolute path");
+        }
+
+        // The stream is available for reading/writing the target's content.
+        stream.setEncoding("utf8");
+        this.stream = stream;
+
+        // The sourcePath is a relative path under the source tree to the target,
+        // excluding the target name.
+        this.sourcePath = sourcePath;
+
+        // The source path to use under the deploy path. This is used by the
+        // implementation.
+        this.deploySourcePath = sourcePath;
+
+        // The deployPath is initially not set. It can be set later to an absolute
+        // path for an output target.
+        this.deployPath = null;
+
+        // The target name serves as the identifier for the target.
+        this.targetName = targetName;
+
+        // Determines whether the target will be recursively cycled through the
+        // build system.
+        this.recursive = false;
+
+        // Options provided by the deployment configuration.
+        this.options = options || {};
+
+        // Used by the implementation to track target handlers.
+        this.handlers = undefined;
     }
 
-    // The stream is available for reading/writing the target's content.
-    stream.setEncoding("utf8");
-    this.stream = stream;
+    // Gets the path to the target relative to the target's source tree. This
+    // includes the target name.
+    getSourceTargetPath() {
+        return pathModule.posix.join(this.sourcePath,this.targetName);
+    }
 
-    // The sourcePath is a relative path under the source tree to the target,
-    // excluding the target name.
-    this.sourcePath = sourcePath;
+    // Gets the path to an output target in a deployment. This is an absolute
+    // path that includes the target name.
+    getDeployTargetPath() {
+        if (!this.deployPath) {
+            throw new Error("Deploy path is not set on target");
+        }
 
-    // The source path to use under the deploy path. This is used by the
-    // implementation.
-    this.deploySourcePath = sourcePath;
+        return pathModule.join(this.deployPath,this.targetName);
+    }
 
-    // The deployPath is initially not set. It can be set later to an absolute
-    // path for an output target.
-    this.deployPath = null;
+    // Sets the deploy path for the target. The deploy path will be relative to
+    // the provided base path.
+    setDeployPath(basePath) {
+        // Verify that the path is absolute.
+        if (!pathModule.isAbsolute(basePath)) {
+            throw new Error("Target deploy path must be an absolute path");
+        }
 
-    // The target name serves as the identifier for the target.
-    this.targetName = targetName;
+        this.deployPath = pathModule.join(basePath,this.deploySourcePath);
+    }
 
-    // Determines whether the target will be recursively cycled through the
-    // build system.
-    this.recursive = false;
+    // Creates an output target that inherits from the parent target.
+    makeOutputTarget(newTargetName,newTargetPath,recursive) {
+        if (!newTargetName) {
+            newTargetName = this.targetName;
+        }
 
-    // Options provided by the deployment configuration.
-    this.options = options || {};
+        if (!newTargetPath) {
+            newTargetPath = this.deploySourcePath;
+        }
+
+        var newTarget = makeOutputTarget(newTargetPath,newTargetName,this.options);
+        newTarget.recursive = recursive;
+
+        return newTarget;
+    }
+
+    // Moves the target through the pipeline unchanged.
+    pass() {
+        var newTarget = new Target(this.sourcePath,this.targetName,this.stream,this.options);
+        newTarget.recursive = false;
+
+        return newTarget;
+    }
+
+    // Applies the default plugin settings to the target.
+    applySettings(pluginSettings) {
+        if (pluginSettings.path) {
+            this.deploySourcePath = pluginSettings.path;
+        }
+    }
+
+    setHandlers(handlers) {
+        this.handlers = handlers;
+    }
 }
-
-// Gets the path to the target relative to the target's source tree. This
-// includes the target name.
-Target.prototype.getSourceTargetPath = function() {
-    return pathModule.posix.join(this.sourcePath,this.targetName);
-};
-
-// Gets the path to an output target in a deployment. This is an absolute path
-// that includes the target name.
-Target.prototype.getDeployTargetPath = function() {
-    if (!this.deployPath) {
-        throw new Error("Deploy path is not set on target");
-    }
-
-    return pathModule.join(this.deployPath,this.targetName);
-};
-
-// Sets the deploy path for the target. The deploy path will be relative to the
-// provided base path.
-Target.prototype.setDeployPath = function(basePath) {
-    // Verify that the path is absolute.
-    if (!pathModule.isAbsolute(basePath)) {
-        throw new Error("Target deploy path must be an absolute path");
-    }
-
-    this.deployPath = pathModule.join(basePath,this.deploySourcePath);
-};
-
-// Creates an output target that inherits from the parent target.
-Target.prototype.makeOutputTarget = function(newTargetName,newTargetPath,recursive) {
-    if (!newTargetName) {
-        newTargetName = this.targetName;
-    }
-
-    if (!newTargetPath) {
-        newTargetPath = this.deploySourcePath;
-    }
-
-    var newTarget = makeOutputTarget(newTargetPath,newTargetName,this.options);
-    newTarget.recursive = recursive;
-
-    return newTarget;
-};
-
-// Moves the target through the pipeline unchanged.
-Target.prototype.pass = function() {
-    var newTarget = new Target(this.sourcePath,this.targetName,this.stream,this.options);
-    newTarget.recursive = false;
-
-    return newTarget;
-};
-
-// Applies the default plugin settings to the target.
-Target.prototype.applySettings = function(pluginSettings) {
-    if (pluginSettings.path) {
-        this.deploySourcePath = pluginSettings.path;
-    }
-};
 
 module.exports = {
     Target: Target,
