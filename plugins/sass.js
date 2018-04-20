2f37fd9a00c38e45b08cb512f3446e46ccffaf49
Roger Gee <roger.gee@tulsalibrary.org>
2018-04-20 10:59:06 -0500

    [sass] Improve handling of module paths
    This commit fixes a number of things to make the sass plugin initially
    functional.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   plugins/sass.js
    #

--- plugins/sass.js
+++ plugins/sass.js
@@ -29,18 +29,46 @@ function resolveModulePath(path,callingPath) {
     return pathModule.join(callingPath,path.substr(pos));
 }
 
-function makeCustomImporter(targets) {
+function makeCustomImporter(targets,moduleBase) {
     var targetMap = {};
 
+    function targetPathToModulePath(targetPath) {
+        if (targetPath.substr(0,moduleBase.length) == moduleBase) {
+            var offset = moduleBase.length;
+            while (offset < targetPath.length && targetPath[offset] == '/') {
+                offset += 1;
+            }
+
+            var modulePath = targetPath.substr(offset);
+        }
+        else {
+            var modulePath = targetPath;
+        }
+
+        return modulePath;
+    }
+
     for (var i = 0;i < targets.length;++i) {
-        targetMap[targets[i].getSourceTargetPath()] = targets[i];
+        // Make the module path relative to the configured moduleBase.
+        var targetPath = targets[i].getSourceTargetPath();
+        var modulePath = targetPathToModulePath(targetPath);
+
+        // Remove trailing extension.
+        modulePath = modulePath.substr(0,modulePath.length-5);
+
+        targetMap[modulePath] = targets[i];
     }
 
     return (url,prev,done) => {
-        var path = resolveModulePath(url,prev);
+        // NOTE: This is a hack around how node-sass handles the prev
+        // path. Since it always evaluates an absolute path, we added a leading
+        // '/' earlier so we can preserve the webdeploy virtual path system.
+        prev = prev.substr(1);
+        var currentPath = targetPathToModulePath(pathModule.parse(prev).dir);
 
+        var path = resolveModulePath(url,currentPath);
         if (path in targetMap) {
-            done({ contents: targetMap[path].content });
+            done({ file: path, contents: targetMap[path].content });
         }
         else {
             done(new Error("Module '" + url + "' does not exist"));
@@ -49,7 +77,10 @@ function makeCustomImporter(targets) {
 }
 
 module.exports = {
+    id: "sass",
     exec: (context,settings) => {
+        settings.moduleBase = settings.moduleBase || "";
+
         var scss = [];
 
         // Find all .scss targets.
@@ -74,7 +105,7 @@ module.exports = {
 
         return Promise.all(promises).then(() => {
             var promises = [];
-            var importFunc = makeCustomImporter(scss);
+            var importFunc = makeCustomImporter(scss,settings.moduleBase);
 
             // Call node-sass on each target, saving the compilation into a new
             // target with ".css" suffix.
@@ -83,17 +114,27 @@ module.exports = {
 
                 var renderPromise = new Promise((resolve,reject) => {
                     nodeSass.render({
+                        file: '/' + target.getSourceTargetPath(),
                         data: target.content,
                         includePaths: [target.sourcePath],
+                        indentedSyntax: false,
                         importer: importFunc
                     }, (err, result) => {
                         if (err) {
                             reject(err);
                         }
                         else {
-                            var match = target.targetName.match(/(.*)\.scss$/);
-                            var newPath = pathModule.join(target.sourcePath,match[1] + ".css");
-                            context.resolveTargets(newPath,[target]);
+                            // Only include the build product if it resolved to actual content.
+                            if (result.css.length > 0) {
+                                var match = target.targetName.match(/(.*)\.scss$/);
+                                var newPath = pathModule.join(target.sourcePath,match[1] + ".css");
+                                var newTarget = context.resolveTargets(newPath,[target]);
+                                newTarget.stream.end(result.css.toString('utf8'));
+                            }
+                            else {
+                                // Remove targets that evaluated to empty.
+                                context.resolveTargets(null,[target]);
+                            }
 
                             resolve();
                         }
